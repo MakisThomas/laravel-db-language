b6eb50672821dfe11cc4fa95f914077b724603ce
Makis Thomas <naoythoma@hotmail.com>
2017-10-05 19:15:22 +0300

    Changes

--- src/views/fields/bootstrap3.blade.php
+++ src/views/fields/bootstrap3.blade.php
@@ -8,7 +8,7 @@
             </label>
         @endif
         <div class="input-group">
-            @foreach($fields as $field)
+            @foreach($fields as $index=>$field)
                 <input type="text"
                        name="{{ $field }}"
                        class="form-control {{ (substr($field, -2) == $defaultLanguage)?'':'hidden' }}"
@@ -20,7 +20,7 @@
             <div class="input-group-btn btn_lang">
                 <button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown"
                         aria-haspopup="true" aria-expanded="false">
-                    <span class="flag-icon flag-icon-{{ Lang::convertLocaleToCountryCode($defaultLanguage) }}"></span>
+                    <i class="flag-icon flag-icon-{{ Lang::convertLocaleToCountryCode($defaultLanguage) }}"></i>
                     <span class="caret"></span></button>
                 <ul class="dropdown-menu dropdown-menu-right">
                     @foreach($fields as $field)
@@ -48,20 +48,22 @@
         <div class="dropdown btn_lang">
             <button class="btn btn-default dropdown-toggle" type="button" id="dropdownMenu1" data-toggle="dropdown"
                     aria-haspopup="true" aria-expanded="true">
-                <span class="flag-icon flag-icon-{{ Lang::convertLocaleToCountryCode($defaultLanguage) }}"></span>
+                <i class="flag-icon flag-icon-{{ Lang::convertLocaleToCountryCode($defaultLanguage) }}"></i>
                 <span class="caret"></span>
             </button>
             <ul class="dropdown-menu" aria-labelledby="dropdownMenu1">
                 @foreach($fields as $field)
                     <li class="{{ (substr($field, -2) == $defaultLanguage)?'active':'' }}">
-                        <a href="#" data-code="{{ substr($field, -2) }}">
+                        <a href="#"
+                            data-code="{{ substr($field, -2) }}"
+                            data-lang_code="{{ Lang::convertLocaleToCountryCode(substr($field, -2)) }}">
                             <span class="flag-icon flag-icon-{{ Lang::convertLocaleToCountryCode(substr($field, -2)) }}"></span>
                         </a>
                     </li>
                 @endforeach
             </ul>
         </div>
-        @foreach($fields as $field)
+        @foreach($fields as $index=>$field)
             <textarea
                     name="{{ $field }}"
                     class="form-control {{ (substr($field, -2) == $defaultLanguage)?'':'hidden' }}"
